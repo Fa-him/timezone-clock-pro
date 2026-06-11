76a6cf672581576816ba2765de831c3fd098bbb1
Fa-him <hisirfs@gmail.com>
2026-06-12 02:31:43 +0600

    base added to vite config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/timezone-clock-pro/',
   plugins: [react()],
   server: {
     port: 5173,
